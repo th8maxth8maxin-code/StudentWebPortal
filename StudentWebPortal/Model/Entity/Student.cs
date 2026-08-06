77d4a2fce747310d56f37ebabd7186f3d61ad76b
th8max <th8max.th8max.in@gmail.com>
2026-08-06 11:52:21 +0530

    Updated the Stundent table Entity

--- StudentWebPortal/Model/Entity/Student.cs
+++ StudentWebPortal/Model/Entity/Student.cs
@@ -1,12 +1,34 @@
-﻿namespace StudentWebPortal.Model.Entity
+﻿using System.ComponentModel.DataAnnotations;
+
+
+
+namespace StudentWebPortal.Model.Entity
+
 {
     public class Student
     {
+
+        [Key]
         public int StudentId { get; set; }
+
+        [Required]
+        [MaxLength(100)]
         public required string StudentName { get; set; }
-        public int PhoneNumber { get; set; }
-        public string?   Email { get; set; }
-        public required string Grade{ get; set; }
-        public required string Attendance  { get; set; }
+
+
+        [Phone]
+        [MaxLength(15)]
+        public string? PhoneNumber { get; set; }
+
+        [EmailAddress]
+        [MaxLength(100)]
+        public string? Email { get; set; }
+
+        [MaxLength(15)]
+        [Required]
+        public required string Grade { get; set; }
+
+        [Range(0, 100)]
+        public decimal Attendance { get; set; }
     }
 }
